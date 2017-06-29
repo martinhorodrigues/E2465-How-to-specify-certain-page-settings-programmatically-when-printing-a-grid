fcc60f3fa2b24dd46d9bfdb35c4510dfc51b8e58
dxBot <DevExpressExampleBot>
2017-06-30 02:32:06 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Windows;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Printing;
+
+namespace WpfPrintGridPageSettings
+{
+    public partial class MainWindow : DXWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            gridControl1.Columns.Clear();
+            gridControl1.DataSource = TabletDataSet.CreateData().Tables[0];
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            VisualDataNodeLink link = new VisualDataNodeLink((DevExpress.Xpf.Grid.TableView)gridControl1.View, "My Document");
+
+            link.PaperKind = System.Drawing.Printing.PaperKind.A5;
+            link.Landscape = true;
+
+            link.ShowPrintPreviewDialog(this);
+        }
+    }
+
+    #region Data for the Grid
+    public class TabletDataSet : DataSet
+    {
+        private const int m_columns = 10;
+        private const int m_rows = 10;
+
+        public TabletDataSet()
+            : base()
+        {
+            DataTable table = new DataTable("table");
+
+            DataSetName = "ManualDataSet";
+
+            for (int i = 0; i < m_columns; i++)
+            {
+                table.Columns.Add("Column" + i.ToString(), typeof(Int32));
+            }
+
+            Tables.AddRange(new DataTable[] { table });
+        }
+
+        public static TabletDataSet CreateData()
+        {
+            TabletDataSet ds = new TabletDataSet();
+            DataTable table = ds.Tables["table"];
+
+            for (int i = 0; i < m_rows; i++)
+            {
+                object[] row = new object[m_columns];
+
+                for (int j = 0; j < m_columns; j++)
+                {
+                    row[j] = i * m_columns + j;
+                }
+
+                table.Rows.Add(row);
+            }
+
+            return ds;
+        }
+
+        #region Disable Serialization for Tables and Relations
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public new DataTableCollection Tables
+        {
+            get { return base.Tables; }
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public new DataRelationCollection Relations
+        {
+            get { return base.Relations; }
+        }
+        #endregion Disable Serialization for Tables and Relations
+    }
+    #endregion Data for the Grid
+
+}
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,85 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Windows
+Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Printing
+
+Namespace WpfPrintGridPageSettings
+	Partial Public Class MainWindow
+		Inherits DXWindow
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			gridControl1.Columns.Clear()
+			gridControl1.DataSource = TabletDataSet.CreateData().Tables(0)
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim link As New VisualDataNodeLink(CType(gridControl1.View, DevExpress.Xpf.Grid.TableView), "My Document")
+
+			link.PaperKind = System.Drawing.Printing.PaperKind.A5
+			link.Landscape = True
+
+			link.ShowPrintPreviewDialog(Me)
+		End Sub
+	End Class
+
+	#Region "Data for the Grid"
+	Public Class TabletDataSet
+		Inherits DataSet
+		Private Const m_columns As Integer = 10
+		Private Const m_rows As Integer = 10
+
+		Public Sub New()
+			MyBase.New()
+			Dim table As New DataTable("table")
+
+			DataSetName = "ManualDataSet"
+
+			For i As Integer = 0 To m_columns - 1
+				table.Columns.Add("Column" & i.ToString(), GetType(Int32))
+			Next i
+
+			Tables.AddRange(New DataTable() { table })
+		End Sub
+
+		Public Shared Function CreateData() As TabletDataSet
+			Dim ds As New TabletDataSet()
+			Dim table As DataTable = ds.Tables("table")
+
+			For i As Integer = 0 To m_rows - 1
+				Dim row(m_columns - 1) As Object
+
+				For j As Integer = 0 To m_columns - 1
+					row(j) = i * m_columns + j
+				Next j
+
+				table.Rows.Add(row)
+			Next i
+
+			Return ds
+		End Function
+
+		#Region "Disable Serialization for Tables and Relations"
+		<DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)> _
+		Public Shadows ReadOnly Property Tables() As DataTableCollection
+			Get
+				Return MyBase.Tables
+			End Get
+		End Property
+
+		<DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)> _
+		Public Shadows ReadOnly Property Relations() As DataRelationCollection
+			Get
+				Return MyBase.Relations
+			End Get
+		End Property
+		#End Region ' Disable Serialization for Tables and Relations
+	End Class
+	#End Region ' Data for the Grid
+
+End Namespace
